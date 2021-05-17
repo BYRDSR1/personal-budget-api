9c27602fe2e0184a1e61f23fc4fdffc615524e41
BYRDSR1 <79857300+BYRDSR1@users.noreply.github.com>
2021-05-17 10:34:42 -0500

    Work on POST route for /update, need to fix xor bug

--- routes/update.js
+++ routes/update.js
@@ -7,7 +7,8 @@ const fs = require("fs");
 const envelopes = require("../db/db.js");
 //Functions
 const {
-	findEnvelope
+	findEnvelope,
+	updateEnvelope
 } = require("../utils/funcs.js")
 
 updateRouter.get("/", (req, res, next) => {
@@ -20,6 +21,9 @@ updateRouter.get("/", (req, res, next) => {
 updateRouter.post("/", (req, res, next) => {
 	const name = req.body.name;
 	const id = parseInt(req.body.id);
+	const newName = req.body.newname;
+	const newAmount = req.body.newamount;
+	
 	if(id) {
 		if(!findEnvelope(id, "object")) {
     	res.render(path.join(__dirname, "..", "views", "pages", "update.ejs"), {
@@ -27,7 +31,19 @@ updateRouter.post("/", (req, res, next) => {
 		    findError: `<h3 style="text-align:center">Unable to find an item with id "${id}"</h3>`
 	    });
 		} else {
-      
+			if(!newAmount XOR !newName) {
+				res.render(path.join(__dirname, "..", "views", "pages", "update.ejs"), {
+					name: "update",
+					findError: `<h3 style="text-align:center">Please enter a new amount or name</h3>`
+				});
+			} else {
+				const newEnvelope = {name: newName, amount: newAmount};
+				updateEnvelope(id, newEnvelope);
+				res.status(201).render(path.join(__dirname, "..", "views", "pages", "update.ejs"), {
+					name: "update",
+					findError: `<h3 style="text-align:center">Envelope updated!</h3>`
+				});
+			}
 		}
 	} else {
 		if(!findEnvelope(name, "object")) {
@@ -41,7 +57,19 @@ updateRouter.post("/", (req, res, next) => {
 		    findError: '<h3 style="text-align:center">There Are More Than One Envelopes with That Name, Please Use the Id</h3>'
 	    });
 		} else {
-			
+			if(!newAmount XOR !newName) {
+				res.render(path.join(__dirname, "..", "views", "pages", "update.ejs"), {
+					name: "update",
+					findError: `<h3 style="text-align:center">Please enter a new amount or new name</h3>`
+				});
+			} else {
+				const newEnvelope = {name: newName, amount: newAmount};
+				updateEnvelope(name, newEnvelope);
+				res.status(201).render(path.join(__dirname, "..", "views", "pages", "update.ejs"), {
+					name: "update",
+					findError: `<h3 style="text-align:center">Envelope updated!</h3>`
+				});
+			}
 		}
 	}
 	next();
