8ad73cf54e4c1e43485fdf7b9fa51171071ada52
Spencer Byrd <spencerlink06@gmail.com>
2021-05-18 20:40:24 -0500

    Add POST handler for /delete and make the frame for it

--- routes/delete.js
+++ routes/delete.js
@@ -10,11 +10,34 @@ const {
 	findEnvelope
 } = require("../utils/funcs.js")
 
-deleteRouter.get("/", (req, res) => {
+deleteRouter.get("/", (req, res, next) => {
 	res.render(path.join(__dirname, "..", "views", "pages", "delete.ejs"), {
 		name: "delete",
-		info: `<h3 style="text-align:center"></h3>`
+		info: `<h3 style="text-align:center">Enter an envelope's name or id</h3>`
 	});
+	next();
+});
+
+deleteRouter.post("/", (req, res, next) => {
+	const name = req.body.name;
+	const id = parseInt(req.body.id);
+	if(id) {
+		if(findEnvelope(id)) {
+			//delete envelope
+		} else {
+			//no envelope with id
+		}
+	} else {
+		if(findEnvelope(name)) {
+			if(findEnvelope(name).length > 1) {
+				//Too many envelopes with name
+			} else {
+				//delete envelope
+			}
+		} else {
+			//no envelope with name
+		}
+	}
 });
 
 module.exports = deleteRouter;
